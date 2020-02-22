8b66ad4d85796c9874a9ad2ded7cc66a167b9364
kimhakyong <kimhakyong@gmail.com>
2020-02-22 21:26:17 +0900

    ...

--- src/main/java/modern/java/in/action/Apple.java
+++ src/main/java/modern/java/in/action/Apple.java
@@ -1,12 +1,21 @@
 package modern.java.in.action;
 
+enum Color {RED, GREEN}
+
 public class Apple {
 	private int weight = 0;
-	private String color = "";
+	private Color color = Color.GREEN;
 
-	public Apple(int weight, String color) {
+	public Apple(int weight, Color color) {
 		this.weight = weight;
 		this.color = color;
+	}
+
+	public Apple() {
+	}
+
+	public Apple(int weight) {
+		this.weight = weight;
 	}
 
 	public int getWeight() {
@@ -17,17 +26,17 @@ public class Apple {
 		this.weight = weight;
 	}
 
-	public String getColor() {
+	public Color getColor() {
 		return color;
 	}
 
-	public void setColor(String color) {
+	public void setColor(Color color) {
 		this.color = color;
 	}
 
 	@SuppressWarnings("boxing")
 	@Override
 	public String toString() {
-		return String.format("Apple{color='%s', weight=%d}", color, weight);
+		return String.format("Apple{color='%d', weight=%d}", color.ordinal(), weight);
 	}
 }
